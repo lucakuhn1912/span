6892e0bffa8b7ed3b43e6cd9b498a69383feba19
Luca Kuhn <luca.kuhn@hotmail.de>
2019-10-06 16:24:52 +0100

    cards on index page

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,5 @@
 class Recipe < ApplicationRecord
+  def self.search(search)
+    where("name ILIKE ?", "%#{search}%")
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-eggs_beneditct = Recipe.create(name: "Eggs Benedict", description: "Two halves of low-carb English muffin topped with a poached egg, ham, and hollandaise sauce", preptime: 15, category: 'breakfast', subcategory: 'egg')
-banana_pancakes = Recipe.create(name: "Banana Pancakes", description: "Gluten-free banana pancakes you can whip up in just 10 minutes! Scatter with pecans and raspberries to enjoy a low-calorie yet indulgent breakfast", preptime: 10, category: 'breakfast', subcategory: 'sweet')
-avo_toast = Recipe.create(name: "Avo On Toast", description: "Smashed avocado on low-carb bread with feta and pomegranate", preptime: 15, category: 'breakfast', subcategory: 'bread')
-acai_bowl = Recipe.create(name: "Acai Smootie Bowl", description: "Thick smoothie bowl made from Acai berries topped with coco nibs, banana and almond butter ", preptime: 5, category: 'breakfast', subcategory: 'smoothie')
-veg_omlette = Recipe.create(name: "Vegteble veg_omlette", description: "3 Egg Omlette with peppers, spinach and mushrooms", preptime: 10, category: 'breakfast', subcategory: 'egg')
+Recipe.destroy_all
+
+eggs_beneditct = Recipe.create(name: "Eggs Benedict", description: "Two halves of low-carb English muffin topped with a poached egg, ham, and hollandaise sauce", preptime: 15, category: 'breakfast', subcategory: 'egg', picture: "eggs-benedict.jpeg")
+banana_pancakes = Recipe.create(name: "Banana Pancakes", description: "Gluten-free banana pancakes you can whip up in just 10 minutes! Scatter with pecans and raspberries to enjoy a low-calorie yet indulgent breakfast", preptime: 10, category: 'breakfast', subcategory: 'sweet', picture: "banana-pancakes.jpg")
+avo_toast = Recipe.create(name: "Avocado On Toast", description: "Smashed avocado on low-carb bread with feta and pomegranate", preptime: 15, category: 'breakfast', subcategory: 'bread', picture: 'avotoast.jpg')
+acai_bowl = Recipe.create(name: "Acai Smoothie Bowl", description: "Thick smoothie bowl made from Acai berries topped with coco nibs, banana and almond butter ", preptime: 10, category: 'breakfast', subcategory: 'smoothie', picture: 'acai.jpg')
+veg_omelette = Recipe.create(name: "Vegetable Omelette", description: "3 Egg Omelette with peppers, spinach and mushrooms", preptime: 10, category: 'breakfast', subcategory: 'egg', picture: 'veg-omlette.jpeg')
+egg_toast = Recipe.create(name: "Egg on Toast", description: "Fried egg on low carb toast with spinach and tomatos", preptime: 17, category: 'breakfast', subcategory: 'egg', picture: 'eggtoast.jpg')
+green_smoothie = Recipe.create(name: "Green Machine Smoothie", description: "Smoothie made with apple, spinach, avocado, banana and pea protein", preptime: 10, category: 'breakfast', subcategory: 'smoothie', picture: 'greens.jpg')
