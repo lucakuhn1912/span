df489d58a77426af8df0da2b8d70dcc98bd78694
Luca Kuhn <luca.kuhn@hotmail.de>
2019-10-08 08:47:07 -0500

    search forms and filters in place

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
-
   def home
+    if params[:search]
+      @recipes = Recipe.search(params[:search]).order("created_at DESC")
+    else
+      @recipes = Recipe.all.order("created_at DESC")
+    end
   end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,11 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.all
     if params[:search]
       @recipes = Recipe.search(params[:search]).order("created_at DESC")
+    elsif params[:category]
+      @recipes = Recipe.all
+    elsif params[:subcategory].present?
+      @recipes = Recipe.search(params[:subcategory]).order("created_at DESC")
     else
       @recipes = Recipe.all.order("created_at DESC")
     end
